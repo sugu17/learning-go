1b16a65d37d00c55c39df5c1ae78891067f9cb76
Wade Wilson <sugumarsg17@gmail.com>
2024-11-20 06:46:50 +0530

    Batman

---
+++ exercise-1.1.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int = 20
+	var f float64
+	f = float64(i)
+	fmt.Println("Value of f is", f)
+}
---
+++ exercise-1.2.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const value = 3
+	var i int = value
+	var f float64 = value
+	fmt.Println("i:", i)
+	fmt.Println("f:", f)
+}
